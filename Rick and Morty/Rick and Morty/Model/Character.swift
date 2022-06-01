8c5f1791c9f0580ea9bba0ca7da15f7f3a5b0486
Scott Cox <jscox5@gmail.com>
2022-06-01 10:13:18 -0600

    model complete

--- Rick and Morty/Rick and Morty/Model/Character.swift
+++ Rick and Morty/Rick and Morty/Model/Character.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+struct TopLevelDictionary {
+    private enum CodingKeys: String, CodingKey {
+        case results
+    }
+    let results: [ResultsDictionary]
+}
+
+struct ResultsDictionary {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case status
+        case species
+        case gender
+        case origin
+        case location
+        case image
+    }
+    let id: Int
+    let name: String
+    let status: String
+    let species: String
+    let gender: String
+    let origin: [OriginDictionary]
+    let location: [LocationDictionary]
+    let image: String
+}
+
+struct OriginDictionary {
+    private enum CodingKeys: String, CodingKey {
+        case name
+    }
+    let name: String
+}
+
+struct LocationDictionary {
+    private enum CodingKeys: String, CodingKey {
+        case name
+    }
+    let name: String
+}
+
+
---
+++ Rick and Morty/Rick and Morty/Networking Controller/NetworkingController.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkingController.swift
+//  Rick and Morty
+//
+//  Created by Scott Cox on 6/1/22.
+//
+
+import Foundation
+
