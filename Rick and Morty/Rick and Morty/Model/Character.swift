5fc5f9c4f17d0d0c9d0243e6832f6f82031b5fe7
Scott Cox <jscox5@gmail.com>
2022-06-01 09:59:34 -0600

    UI complete

---
+++ Rick and Morty/Rick and Morty/Model/Character.swift
@@ -0,0 +1,8 @@
+//
+//  Character.swift
+//  Rick and Morty
+//
+//  Created by Scott Cox on 6/1/22.
+//
+
+import Foundation
---
+++ Rick and Morty/Rick and Morty/View Controllers/CharacterDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CharacterDetailsViewController.swift
+//  Rick and Morty
+//
+//  Created by Scott Cox on 6/1/22.
+//
+
+import UIKit
+
+class CharacterDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
