760acea2bbdb2fd60335186bdcf4e9ac4b526d65
Scott Cox <jscox5@gmail.com>
2022-06-01 11:33:39 -0600

    Network Controller complete

--- Rick and Morty/Rick and Morty/Networking Controller/NetworkingController.swift
+++ Rick and Morty/Rick and Morty/Networking Controller/NetworkingController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit.UIImage
+import UIKit
 
 class NetworkingController {
     
@@ -34,6 +35,80 @@ class NetworkingController {
     }
     
     // MARK: - Endpoint 2
+    static func fetchCharacter(with characterURLString: String, completion: @escaping(Result<ResultsDictionary, ResultError>) -> Void) {
+        guard let characterURL = URL(string: characterURLString) else {
+            completion(.failure(.invalidURL(URL(string: characterURLString)!)))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: characterURL) { data, _, error  in
+            if let error = error {
+                print("Encountered Error: \(error.localizedDescription)")
+                completion(.failure(.thrownError(error)))
+            }
+            guard let characterData = data else {
+                completion(.failure(.noData))
+                return
+            }
+            do {
+                let character = try JSONDecoder().decode(ResultsDictionary.self, from: characterData)
+                completion(.success(character))
+            } catch {
+                print("Encountered error when decoding the data:", error.localizedDescription)
+                completion(.failure(.unabletoDecode))
+            }
+        }.resume()
+    }
     
+    // MARK: - Endpoint 3
+    static func fetchOrigin(with originURLString: String, completion: @escaping(Result<OriginDictionary, ResultError>) -> Void) {
+        guard let originURL = URL(string: originURLString) else {
+            completion(.failure(.invalidURL(URL(string: originURLString)!)))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: originURL) { data, _, error in
+            if let error = error {
+                print("Encountered Error: \(error.localizedDescription)")
+                completion(.failure(.thrownError(error)))
+            }
+            guard let originData = data else {
+                completion(.failure(.noData))
+                return
+            }
+            do {
+                let origin = try JSONDecoder().decode(OriginDictionary.self, from: originData)
+                completion(.success(origin))
+            } catch {
+                print("Encountered error when decoding the data:", error.localizedDescription)
+                completion(.failure(.unabletoDecode))
+            }
+        }.resume()
+    }
     
-}
+    // MARK: - Endpoint 4
+    static func fetchLocation(with locationURLString: String, completion: @escaping(Result<LocationDictionary, ResultError>) -> Void) {
+        guard let locationURL = URL(string: locationURLString) else {
+            completion(.failure(.invalidURL(URL(string: locationURLString)!)))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: locationURL) { data, _, error in
+            if let error = error {
+                print("Encountered Error: \(error.localizedDescription)")
+                completion(.failure(.thrownError(error)))
+            }
+            guard let locationData = data else {
+                completion(.failure(.noData))
+                return
+            }
+            do {
+                let location = try JSONDecoder().decode(LocationDictionary.self, from: locationData)
+                completion(.success(location))
+            } catch {
+                print("Encountered error when decoding the data:", error.localizedDescription)
+                completion(.failure(.unabletoDecode))
+            }
+        }.resume()
+    }
+} //END of class
